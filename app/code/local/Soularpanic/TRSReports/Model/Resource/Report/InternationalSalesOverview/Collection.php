9dd058052cc0e44d6f9f91a70df2630b2d68ffa1
Joshua Kossoff <kossoff.j@gmail.com>
2015-08-11 14:54:01 -0400

    Cleaning up dead code.

--- app/code/local/Soularpanic/TRSReports/Model/Resource/Report/InternationalSalesOverview/Collection.php
+++ app/code/local/Soularpanic/TRSReports/Model/Resource/Report/InternationalSalesOverview/Collection.php
@@ -35,7 +35,6 @@ class Soularpanic_TRSReports_Model_Resource_Report_InternationalSalesOverview_Co
         $_select = $this->getSelect();
 
         $_internationalSelect = $_helper->_getNewSelect();
-//        $_ordersSelect = $_helper->getProductOrders($this->_from, $this->_to);
 
         $_internationalAlias = self::TOP_LEVEL_TABLE_ALIAS;
         $_orderAlias = 'orders';
@@ -57,8 +56,7 @@ class Soularpanic_TRSReports_Model_Resource_Report_InternationalSalesOverview_Co
             ->group("$_addressAlias.country_id");
 
         $_select->from([ $_internationalAlias => $_internationalSelect ],
-            [ //"region" => "country_id",
-                "order_count",
+            [ "order_count",
                 "sold_value",
                 "credit_value",
                 "total_value",
@@ -67,28 +65,6 @@ class Soularpanic_TRSReports_Model_Resource_Report_InternationalSalesOverview_Co
                 "$_countryAlias.country_id = $_internationalAlias.country_id",
                 [ 'region' => 'name' ]);
 
-//        $_select->from([ $_xAlias => $_ordersSelect ],
-//            '*');
-
-//        $_taxRate = self::LOCAL_TAX_RATE;
-//        $_region = self::REGION;
-//        $_statuses = "'".implode('\', \'', $this->_statusWhitelist)."'";
-//
-//        $_ordersAlias = self::TOP_LEVEL_TABLE_ALIAS;
-//        $_addressAlias = 'addresses';
-//
-//        $_select->from([ $_ordersAlias => $this->getTable('sales/order') ],
-//            [ 'subtotal' => 'sum(base_subtotal)',
-//                'tax_total' => 'sum(base_tax_amount)',
-//                'calculated_taxed_sales' => "sum(base_tax_amount) / {$_taxRate}",
-//                'calculated_tax_exempt_sales' => "sum(base_subtotal) - (sum(base_tax_amount) / {$_taxRate})" ])
-//            ->joinLeft([ $_addressAlias => $this->getTable('sales/order_address') ],
-//                "$_addressAlias.parent_id = $_ordersAlias.entity_id",
-//                [ 'region' ])
-//            ->where("$_addressAlias.region = '$_region'")
-//            ->where("$_addressAlias.address_type = 'shipping'")
-//            ->where("$_ordersAlias.created_at between '$this->_from' and '$this->_to'")
-//            ->where("status in($_statuses)");
 
         $this->log("International Sales SQL:\n".$this->getSelect()->__toString());
     }
@@ -99,10 +75,6 @@ class Soularpanic_TRSReports_Model_Resource_Report_InternationalSalesOverview_Co
         if ($_field !== null) {
             $this->getSelect()->order(array("{$_field} {$_dir}"));
         }
-//        $customFilterData = $this->getCustomFilterData();
-//        $customFilterData->setProductTable(self::TOP_LEVEL_TABLE_ALIAS);
-//
-//        return parent::_applyCustomFilter();
     }
 
     protected function _applyStoresFilterToSelect(Zend_Db_Select $select) {
